254178038648529e20bb0cd0d92818ceaeca4469
Colin Wee <wee@battelle.org>
2017-09-27 12:13:30 -0400

    start prelim arg processing structure

--- jd2_ffmpeg_m4a.py
+++ jd2_ffmpeg_m4a.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python3
 
+import sys
 from argparse import ArgumentParser
 
-ap = ArgumentParser()
-ap.add_argument("-i", action="append")
-args = ap.parse_args()
 
-print(str(args))
-args.i[1] = "YAAS"
-print(str(args))
+def capture_args():
+    ap = ArgumentParser()
+    ap.add_argument("ff_args", nargs="*")
+    return ap.parse_args()
+
+
+def main():
+    args = capture_args()
+
+    new_args = []
+    fmt_arg = False
+    for arg in args.ff_args:
+        if arg == "-f":
+            fmt_arg = True
+            #  drop arg
+
+        elif fmt_arg:
+            pass
+        
+        else:
+            new_args.append(arg)
+    
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
