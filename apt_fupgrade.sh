0423ea84a66773cbe7e89ac770b8503004007203
Colin Wee <wee@battelle.org>
2018-06-05 13:21:31 -0400

    idk I added stuff

--- apt_fupgrade.sh
+++ apt_fupgrade.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-set -e
+set -ex
+
+# var=0
+# if []
 
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y autoremove
+
+exit
+
+upgradable=0
+sudo apt update | while read line; do
+  echo "$line"
+
+  if [ "$upgradable" == 1 ]; then
+    continue
+  elif echo "$line" | grep -q "can be upgraded. Run 'apt list --upgradable' to see them."; then
+    upgradable=0
+  fi
+done
