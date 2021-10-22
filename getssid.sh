6d20e086b6af3e92de59aad5d6c7a0e4362f78a3
Colin Wee <2gbleh@gmail.com>
2021-10-21 21:59:52 -0700

    getssid: support many different backends

--- getssid.sh
+++ getssid.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-iw dev wlp2s0 link | grep -Po 'SSID: \K.*'
+# Print currently connected SSID
+
+set -e
+
+
+whichq ()
+{
+  which "$@" > /dev/null 2>&1
+}
+
+
+if whichq iwgetid; then
+  iwgetid --raw
+
+elif whichq iw; then
+  iw dev wlp2s0 link | grep -Po 'SSID: \K.*'
+
+elif whichq ip nmcli; then
+  device=$(ip route | grep -Po "default.*dev \K\w+")
+  nmcli -t connection show --active | grep -Fw "$device" | cut -d : -f 1
+
+else
+  >&2 echo "insufficient supported tools"
+  exit 1
+fi
