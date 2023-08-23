59763c85d01effd3f295e88c58734b96cc4b3601
Colin Wee <2gbleh@gmail.com>
2023-08-22 18:17:05 -0700

    add tool to recursively rmdir
    rmdir -p has no "upper limit". This tool allows you to specify a root,
    only removing its subdirectories if they are empty.
    
    This is a minimal implementation. Printing indents doesn't really work,
    and logging could be improved.

---
+++ rmdir_recursive/src/main.rs
@@ -0,0 +1,45 @@
+fn multiply_string_by_integer(string: &str, integer: i32) -> String
+{
+    let mut new_string = String::new();
+
+    for _ in 0..integer {
+        new_string.push_str(string);
+    }
+
+    return new_string;
+}
+
+
+fn rmdir_recursive(dpath: &str, depth: i32) -> anyhow::Result<()>
+{
+    for entry in std::fs::read_dir(dpath)? {
+        let entry = entry?;
+        let entry_type = entry.file_type()?;
+        
+        if entry_type.is_dir() {
+            if let Some(subdir) = entry.path().to_str() {
+                let indent = multiply_string_by_integer("    ", depth);
+                println!("{}{}", indent, subdir);
+                if let Err(e) = rmdir_recursive(subdir, depth + 1) {
+                    return Err(e);
+                }
+            } else {
+                return Err(anyhow::anyhow!("found non utf-8 path"));
+            }
+        }
+    }
+    return Ok(std::fs::remove_dir(dpath)?);
+}
+
+
+fn main() -> anyhow::Result<()>
+{
+    for arg in std::env::args().skip(1) {
+        println!("{}", arg);
+        if let Err(e) = rmdir_recursive(&arg, 0) {
+            return Err(e);
+        }
+    }
+
+    return Ok(());
+}
