07026b44f13e857e95a44e0147e95769f3fda724
Colin Wee <2gbleh@gmail.com>
2024-02-23 14:58:37 -0800

    tv_titles: remove dependency on titlecase
    It's unnecessary, and only provides a minor convenience. Not sure if all
    Wikipedia titles are titlecase'd the same way as this function, so
    instead of refactoring to check whether we 404 and use the raw input,
    we'll just remove the convenience. Development seems to have stalled,
    and the AUR package is orphaned.

--- media/tv_titles.py
+++ media/tv_titles.py
@@ -6,7 +6,6 @@ import urllib.parse
 import urllib.request
 
 from bs4 import BeautifulSoup
-from titlecase import titlecase
 
 
 class WikiShowLookup:
@@ -14,7 +13,7 @@ class WikiShowLookup:
     wiki_article_url_base = "http://en.wikipedia.org/wiki/"
 
     def __init__(self, show_name, use_cache=True):
-        self.show_name = titlecase(show_name)
+        self.show_name = show_name
         self.use_cache = use_cache
 
         escaped = urllib.parse.quote(self.show_name.replace(' ', '_'))
--- media/tv_titles_test.py
+++ media/tv_titles_test.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
 import time
-from titlecase import titlecase
 
 from tv_titles import get_episode_title
 
 def unittest_eps_title_lu(show, season, eps, expected):
-    print(' '*4, "{} S{:02}E{:02}:".format(titlecase(show), season, eps), end=' ')
+    print(' '*4, "{} S{:02}E{:02}:".format(show, season, eps), end=' ')
     retval = get_episode_title(show, season, eps)
     print(retval.encode('utf-8'))
     assert(retval == expected)
@@ -15,35 +14,34 @@ def test_eps_title_lu():
     print(' '*2, "Testing get_episode_title()...")
     start = time.time()
 
-    unittest_eps_title_lu("adventure time", 6, 13, "Thanks for the Crabapples, Giuseppe!")
-    # unittest_eps_title_lu("adventure time", 5, 52, "Billy's Bucket List")
+    unittest_eps_title_lu("Adventure Time", 6, 13, "Thanks for the Crabapples, Giuseppe!")
+    # unittest_eps_title_lu("Adventure Time", 5, 52, "Billy's Bucket List")
 
     print(' '*4, "The Legend of Korra S02E08:", end=' ')
-    r = get_episode_title("the legend of korra", "two", 8)
+    r = get_episode_title("The Legend of Korra", "two", 8)
     print(r)
     assert(r == "Beginnings, Part 2")
 
-    # unittest_eps_title_lu("adventure time", 5, 51, "Lemonhope (Part 1)")
-    # unittest_eps_title_lu("gintama", 1, 4, "Watch Out! Weekly Shonen JUMP Sometimes Comes Out On Saturdays!")
+    # unittest_eps_title_lu("Adventure Time", 5, 51, "Lemonhope (Part 1)")
+    # unittest_eps_title_lu("Gintama", 1, 4, "Watch Out! Weekly Shonen JUMP Sometimes Comes Out On Saturdays!")
 
     # Demoing the cache
-    unittest_eps_title_lu("silicon valley", 2, 9, "Binding Arbitration")
-    unittest_eps_title_lu("the big bang theory", 5, 21, "The Hawking Excitation")
-    unittest_eps_title_lu("archer", 2, 11, "Jeu Monégasque")
+    unittest_eps_title_lu("Silicon Valley", 2, 9, "Binding Arbitration")
+    unittest_eps_title_lu("The Big Bang Theory", 5, 21, "The Hawking Excitation")
+    unittest_eps_title_lu("Archer", 2, 11, "Jeu Monégasque")
 
-    unittest_eps_title_lu("archer", 3, 1, "Heart of Archness: Part I")
-    unittest_eps_title_lu("the big bang theory", 6, 9, "The Parking Spot Escalation")
-    unittest_eps_title_lu("silicon valley", 2, 10, "Two Days of the Condor")
-    unittest_eps_title_lu("the big bang theory", 4, 8, "The 21-Second Excitation")
-    # unittest_eps_title_lu("the big bang theory", 2, 6, "The Cooper-Nowitzki Theorem")
-    unittest_eps_title_lu("the big bang theory", 2, 6, "The Cooper–Nowitzki Theorem")
-    unittest_eps_title_lu("archer", 5, 13, "Arrivals/Departures")
-    unittest_eps_title_lu("archer", 3, 13, "Space Race: Part II")
-    unittest_eps_title_lu("silicon valley", 1, 7, "Proof of Concept")
-    unittest_eps_title_lu("archer", 3, 3, "Heart of Archness: Part III")
-    unittest_eps_title_lu("the big bang theory", 8, 24, "The Commitment Determination")
-    unittest_eps_title_lu("archer", 3, 12, "Space Race: Part I")
-    unittest_eps_title_lu("archer", 3, 2, "Heart of Archness: Part II")
+    unittest_eps_title_lu("Archer", 3, 1, "Heart of Archness: Part I")
+    unittest_eps_title_lu("The Big Bang Theory", 6, 9, "The Parking Spot Escalation")
+    unittest_eps_title_lu("Silicon Valley", 2, 10, "Two Days of the Condor")
+    unittest_eps_title_lu("The Big Bang Theory", 4, 8, "The 21-Second Excitation")
+    unittest_eps_title_lu("The Big Bang Theory", 2, 6, "The Cooper–Nowitzki Theorem")
+    unittest_eps_title_lu("Archer", 5, 13, "Arrivals/Departures")
+    unittest_eps_title_lu("Archer", 3, 13, "Space Race: Part II")
+    unittest_eps_title_lu("Silicon Valley", 1, 7, "Proof of Concept")
+    unittest_eps_title_lu("Archer", 3, 3, "Heart of Archness: Part III")
+    unittest_eps_title_lu("The Big Bang Theory", 8, 24, "The Commitment Determination")
+    unittest_eps_title_lu("Archer", 3, 12, "Space Race: Part I")
+    unittest_eps_title_lu("Archer", 3, 2, "Heart of Archness: Part II")
 
     elapsed = time.time() - start
     print(' '*2, "Passed in {:.3f} seconds".format(elapsed))
