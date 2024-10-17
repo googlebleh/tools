d8b0ab2a0e50a3a6b8071d41a7528f9fff4cf8af
Colin Wee <2gbleh@gmail.com>
2024-10-17 13:46:54 -0700

    workstation: hotfix to recognize work desk display
    This stopped working at some point, needed a fix to match my desk setup.
    Judging by the similarity, the preceeding line is likely supposed to
    match, but no time to debug that now.

--- cwee-HP-ZBook-Studio-G7/notification_location.sh
+++ cwee-HP-ZBook-Studio-G7/notification_location.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-
 set -e
 
 declare -a topleft_regexes=(
     "connected primary 3840x2160+0+0"
     "HDMI-1-0 connected 3840x2160+0+0"
     "DP-1-0 connected .\+3440x1440+0+0"
+    "DP-1-0 connected 3840x2160+0+0"
 )
 
 if xrandr | grep -q "${topleft_regexes[@]/#/--regexp=}"; then
