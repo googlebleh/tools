8bb60fc1b5959384db4bc70c839fff6676834d8c
Colin Wee <2gbleh@gmail.com>
2025-04-22 18:20:52 -0700

    Add tool to build the latest LLVM from source

---
+++ build_llvm.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -ex
+
+curl -s https://api.github.com/repos/llvm/llvm-project/releases/latest \
+	| jq -r ".assets[].browser_download_url" \
+	| grep -E "llvm-project-[0-9.]+\.src\.tar\.xz$" \
+	| xargs curl -L \
+	| tar -xJf -
+
+src_glob=llvm-project-*/llvm
+build_dpath=build-llvm-project
+install_prefix=/usr
+cmake \
+	-S $src_glob \
+	-B "$build_dpath" \
+	-G Ninja \
+	-DLLVM_PARALLEL_LINK_JOBS=2 \
+	-DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" \
+	-DCMAKE_BUILD_TYPE=Release
+cmake --build "$build_dpath" --parallel 32
+cmake --install "$build_dpath" "--prefix=$install_prefix"
+
+rm -rf $src_glob "$build_dpath"
